68079f6181f7d8f26fad6e8756db9ab23cc442b9
Egor <ivashkov.egor.e@gmail.com>
2025-08-14 22:32:05 +0700

    Localization changes, added github profile link

--- scripts/lang.js
+++ scripts/lang.js
@@ -5,16 +5,18 @@ let langBtn = document.querySelector('#lang-btn');
 let langDict = {
     'en': {
         'lang': 'EN',
-        'title': 'Hi, it\'s <b>Egor Ivashkov</b>',
-        'description': 'and this is my personal website that is <i>currently under development</i>.',
+        'title': 'My name is <b>Egor Ivashkov</b>',
+        'description': 'this is my personal website that is <i>currently under development</i>.',
         'telegram': 'Telegram',
+        'github': 'GitHub',
         'email': 'Email'
     },
     'ru': {
         'lang': 'RU',
-        'title': 'Привет, меня зовут <b>Ивашков Егор</b>',
-        'description': 'и это мой личный сайт, который <i>в настоящее время находится в разработке</i>.',
+        'title': 'Меня зовут <b>Ивашков Егор</b>',
+        'description': 'это мой личный сайт, который <i>в настоящее время находится в разработке</i>.',
         'telegram': 'Телеграм',
+        'github': 'GitHub',
         'email': 'Электронная почта'
     }
 };
